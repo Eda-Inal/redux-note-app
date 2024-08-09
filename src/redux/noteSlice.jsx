4df94e73f8eb6d2936c215d5c6142778685a732b
Eda Işıl İnal <edainalisil@gmail.com>
2024-08-09 16:30:29 +0300

    created redux react and material ui

---
+++ src/redux/noteSlice.jsx
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const noteSlice = createSlice({
+    name : 'note',
+    initialState : {
+        value:0
+    },
+    reducers : {
+
+    }
+})
+
+// export const {}  = noteSlice.actions 
+export default noteSlice.reducer
