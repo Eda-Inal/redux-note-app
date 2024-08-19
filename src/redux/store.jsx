c27d5e3cfcc5ea7f5634a5dbf80e767bb0b9465f
Eda Işıl İnal <edainalisil@gmail.com>
2024-08-19 14:25:59 +0300

    add  noteslice to local storage

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,7 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
-import noteReducer from "./noteSlice"
+import { configureStore } from '@reduxjs/toolkit';
+import noteReducer from './noteSlice';
+import { saveState, loadState } from '../localstorage';
+
+const preloadedState = {
+    note: loadState() || undefined 
+};
+
 export const store = configureStore({
-  reducer: {
-    note : noteReducer
-  },
-})
+    reducer: {
+        note: noteReducer
+    },
+    preloadedState, 
+});
+
+store.subscribe(() => {
+    saveState(store.getState().note); 
+});
